2cdb12c75c4e8bceceb2267a2975564516c8a836
Esther Pont <esther.cescit@gmail.com>
2025-04-11 09:32:16 +0200

    Part 07.02 - Non null assertion operator (!)

---
+++ fundamentals/07-02-non-null-assertion-operator.ts
@@ -0,0 +1,7 @@
+let courseId: number | null;
+//La variable podria ser null, pero ara mateix 100% no o es (!)
+courseId!.toString();
+
+/*Es pot fer servir en casos que tinguis clar 
+* que la variable no és null en aquest moment
+*/
