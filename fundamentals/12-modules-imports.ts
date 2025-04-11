790054bcb2df1b0e0b33447e8c0abc75ae0e6ce7
Esther Pont <esther.cescit@gmail.com>
2025-04-11 14:51:31 +0200

    Part 15 - Default exports

--- fundamentals/12-modules-imports.ts
+++ fundamentals/12-modules-imports.ts
@@ -3,5 +3,17 @@ import {PAGE_SIZE, COURSE} from "./12-modules-exports";
 const pageSize = PAGE_SIZE;
 
 
+//Part 14
 //Si importem desde index no fa falta posar tot el path
 import {Course, loadAllCourses, saveCourse} from "./14-module-reexports";
+
+
+//Part15
+//Importa el export per defecte (default).
+//Es pot posar cualsevol nom, però millor el mateix. 
+import printCourse from "./15-default-exports";
+printCourse({});
+
+//S'importa tot el que no sigui default en un objecte anomenat constants.
+import * as constants from "./15-default-exports";
+constants.COURSE_TOTAL;
---
+++ fundamentals/15-default-exports.ts
@@ -0,0 +1,12 @@
+export const COURSE_TOTAL = 20;
+
+export const TYPESCRIPT_COURSE = {
+    title: "hello",
+    subtitle: "sub",
+    lessonCount: 10
+};
+
+//Exporta una única cosa principal
+export default function printCourse(course) {
+    console.log(`the course title is ${course}`);
+}
