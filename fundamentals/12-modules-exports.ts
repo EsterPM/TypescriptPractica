238133d623cefefe6aedf96f4eb880dd5eec2e68
Esther Pont <esther.cescit@gmail.com>
2025-04-11 11:33:54 +0200

    Part 12 - Export Import

---
+++ fundamentals/12-modules-exports.ts
@@ -0,0 +1,14 @@
+export type Course = {
+    readonly title:string,
+    subtitle:string,
+    lessonsCount?:number
+};
+
+export const PAGE_SIZE = 100;
+const pageSize = PAGE_SIZE;
+
+export const COURSE = {
+    title: "Hello",
+    subtitle: "sub",
+    lessonsCount: 10
+};
---
+++ fundamentals/12-modules-imports.ts
@@ -0,0 +1,3 @@
+import {PAGE_SIZE, COURSE, Course} from "./12-modules-exports";
+
+const pageSize = PAGE_SIZE;
