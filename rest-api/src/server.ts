668c19c9f30742ac7d617d0004d30e42131822ad
Esther Pont <esther.cescit@gmail.com>
2025-04-15 15:39:55 +0200

    Rest API: Express inici

--- rest-api/src/server.ts
+++ rest-api/src/server.ts
@@ -1,2 +1,16 @@
+import * as express from 'express';
 
-console.log("Hello");
+//Aquesta constant app representa el nostre servidor web.
+const app = express();
+
+//configurar les rutes del servidor
+function setupExpress() {
+
+    //pàgina principal
+    app.route("/").get(root);
+}
+
+//iniciar el servidor i escoltar peticions.
+function startServer() {
+
+}
