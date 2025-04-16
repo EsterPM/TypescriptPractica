e682e3fc767270bbe729a6b1559222d0dd1916a1
Esther Pont <esther.cescit@gmail.com>
2025-04-16 16:40:38 +0200

    Rest API: Omplir la bbdd amb dades de prova

--- rest-api/src/models/populate-db.ts
+++ rest-api/src/models/populate-db.ts
@@ -7,15 +7,58 @@ import "reflect-metadata";
 //dades de prova
 import {COURSES} from "./db-data";
 import {AppDataSource} from "../data-source";
+import { Course } from "./course";
+//permet crear un objecte amb totes les propietats opcionals d'una entitat.
+import {DeepPartial} from "typeorm";
+import {Lesson} from "./lesson";
 
 
-//inicialitza la connexió amb la base de dades
 async function populateDb() {
 
     await AppDataSource.initialize();
 
     console.log(`Database connection ready.`);
 
+    //llista de cursos extrets de les dades d'exemple (COURSES)
+    const courses = Object.values(COURSES) as DeepPartial<Course>[];
+
+    //repositoris creats per gestionar les entitats
+    const courseRepository = AppDataSource.getRepository(Course);
+    const lessonsRepository = AppDataSource.getRepository(Lesson);
+
+    //Inserció de cursos i lliçons
+    for (let courseData of courses) {
+        //Per cada curs, es crea un nou objecte
+        console.log(`Inserting course ${courseData.title}`);
+        const course = courseRepository.create(courseData);
+
+        //s'inserta a la base de dades
+        await courseRepository.save(course);
+
+        for (let lessonData of courseData.lessons) {
+
+            console.log(`Inserting lesson ${lessonData.title}`);
+
+            const lesson = lessonsRepository.create(lessonData);
+
+            lesson.course = course;
+
+            await lessonsRepository.save(lesson);
+        }
+
+    }
+
+
+    //es compta el nombre total de cursos i lliçons a la base de dades
+    const totalCourses = await courseRepository
+        .createQueryBuilder()
+        .getCount();
+
+    const totalLessons = await lessonsRepository
+        .createQueryBuilder()
+        .getCount();
+
+    console.log(` Data Inserted - courses ${totalCourses}, lessons ${totalLessons}`);
 
 }
 
